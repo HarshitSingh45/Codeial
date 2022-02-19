30767ee3a0a855d087f15bbbba8414b09ea035e5
harshitsingh45 <harshitsingh45@gmail.com>
2022-02-19 10:35:06 +0530

    added signup and signin pages

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -17,3 +17,13 @@ module.exports.signin = function(req,res){
         title: 'Codeial | SignIn'
     })
 }
+
+// create action
+module.exports.create = function(req, res){
+    // TODO later
+}
+
+// create session acion
+module.exports.createSession = function(req, res){
+    // TODO later
+}
