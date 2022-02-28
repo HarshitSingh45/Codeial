3a2661d8dbebcb509df201c51b712813004ae5b8
harshitsingh45 <harshitsingh45@gmail.com>
2022-02-28 14:18:26 +0530

    authorized users can delete their post

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comments = require('../models/comments');
 
 module.exports.createPost = function(req, res){
     console.log('Creating post');
@@ -9,5 +10,20 @@ module.exports.createPost = function(req, res){
         if(err){console.log('error in creating post', err); return}
         return res.redirect('back');
     })
+}
 
+module.exports.destroy = function(req, res){
+    Post.findById(req.params.id, function(err, post){
+        if(err){console.log(`Error in finding the user ${err}`); return}
+        if(post){
+            if(req.user.id == post.user){
+                post.remove();
+                Comments.deleteMany({ post: req.params.id}, function(err){
+                    return res.redirect('back');
+                });
+            }
+        }else{
+            res.redirect('back');
+        }
+    })
 }
