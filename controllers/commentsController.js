b25b8f72b726e25dc38f163651ad7072ae60084e
harshitsingh45 <harshitsingh45@gmail.com>
2022-02-28 15:05:47 +0530

    Authorized user can delete their comment

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -1,5 +1,6 @@
 const Comment = require('../models/comments');
 const Post = require('../models/post');
+const { post } = require('../routes/posts');
 
 module.exports.create = function(req, res){
     Post.findById(req.body.post, function(err, post){
@@ -18,4 +19,21 @@ module.exports.create = function(req, res){
             })
         }
     })
+}
+
+module.exports.destroy = function(req, res){
+    Comment.findById(req.params.id , function(err, comment){
+        if(comment){
+            if(req.user.id == comment.user){
+                let postId = comment.post;
+                comment.remove();
+
+                Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}}, function(err,post){
+                    return res.redirect('back');
+                })
+            }
+        }else{
+            return res.redirect('back');
+        }
+    })   
 }
