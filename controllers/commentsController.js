82ae083806e82f8876f2112950b316c3b5827320
harshitsingh45 <harshitsingh45@gmail.com>
2022-03-01 14:10:21 +0530

    updated code using async await

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -2,27 +2,31 @@ const Comment = require('../models/comments');
 const Post = require('../models/post');
 const { post } = require('../routes/posts');
 
-module.exports.create = function(req, res){
-    Post.findById(req.body.post, function(err, post){
+module.exports.create = async function(req, res){
+    try{
+        let post = await Post.findById(req.body.post);
         if(post){
-            Comment.create({
+            let comment = await Comment.create({
                 content: req.body.content,
                 // post: post._id or
                 post: req.body.post,
                 user: req.user._id
-            }, function(err, comment){
-                // handle error
-
-                post.comments.push(comment);
-                post.save();
-                res.redirect('/');
-            })
+            });
+            post.comments.push(comment);
+            post.save();
+            res.redirect('/');
         }
-    })
+    }catch(err){
+        console.log(`Error occured in creating the comment ${err}`);
+        return;
+    }
+    
+    
 }
 
-module.exports.destroy = function(req, res){
-    Comment.findById(req.params.id , function(err, comment){
+module.exports.destroy = async function(req, res){
+    try{
+        let comment = await Comment.findById(req.params.id);
         if(comment){
             if(req.user.id == comment.user){
                 let postId = comment.post;
@@ -32,8 +36,13 @@ module.exports.destroy = function(req, res){
                     return res.redirect('back');
                 })
             }
-        }else{
-            return res.redirect('back');
+            else{
+                return res.redirect('back');
+            }
         }
-    })   
+    }catch(err){
+        console.log(`Error occured in deleting comment ${err}`);
+        return;
+    }
+      
 }
