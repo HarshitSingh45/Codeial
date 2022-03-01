7db4e40001d3d0c7dc1dc5c89dec752de111ac30
harshitsingh45 <harshitsingh45@gmail.com>
2022-03-01 14:22:09 +0530

    changed code in postController using async await

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,20 +1,23 @@
 const Post = require('../models/post');
 const Comments = require('../models/comments');
 
-module.exports.createPost = function(req, res){
-    console.log('Creating post');
-    Post.create({
-        content : req.body.content,
-        user: req.user._id
-    },function(err, post){
-        if(err){console.log('error in creating post', err); return}
+module.exports.createPost = async function(req, res){
+    try{
+        console.log('Creating post');
+        let post = await Post.create({
+            content : req.body.content,
+            user: req.user._id
+        });
         return res.redirect('back');
-    })
+    }catch(err){
+        console.log('error in creating post', err); 
+        return;
+    }   
 }
 
-module.exports.destroy = function(req, res){
-    Post.findById(req.params.id, function(err, post){
-        if(err){console.log(`Error in finding the user ${err}`); return}
+module.exports.destroy = async function(req, res){
+    try{
+        let post = await Post.findById(req.params.id);
         if(post){
             if(req.user.id == post.user){
                 post.remove();
@@ -25,5 +28,9 @@ module.exports.destroy = function(req, res){
         }else{
             res.redirect('back');
         }
-    })
+    }catch(err){
+        console.log(`Error in deleting the post ${err}`);
+        return;
+    }
+    
 }
