a570c72afb62ab7d03c1763de9b4003635a2c160
harshitsingh45 <harshitsingh45@gmail.com>
2022-03-08 17:07:53 +0530

    jwt token creation

---
+++ controllers/api/v1/users_api.js
@@ -0,0 +1,28 @@
+const User = require('../../../models/users');
+const jwt = require('jsonwebtoken');
+
+
+module.exports.createSession = async function(req, res){
+    // whenever a username password is received , we need to find the user and generate the json webtoken corresponding to that user
+    try{
+        let user = await User.findOne({email: req.body.email});
+
+        if(!user || user.password != req.body.password){
+            return res.json(422, {
+                message: 'Ivalid username or password'
+            });
+        }
+        return res.json(200, {
+            message: 'Signedin successfully, here is your token keep it safe bitch !!',
+            data: {
+                token: jwt.sign(user.toJSON(), 'codeial', {expiresIn: '100000'})
+            }
+        })
+    }catch(err){
+        console.log(`********** ${err}`);
+        return res.json(500,{
+            message: 'Internal server error'
+        });
+    }
+
+}
