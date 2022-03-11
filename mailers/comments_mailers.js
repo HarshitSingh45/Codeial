c604852128e7525f2d722ae5ff3ea1b3cb8270c6
harshitsingh45 <harshitsingh45@gmail.com>
2022-03-11 16:30:20 +0530

    html template emails can sent when any action occurs

--- mailers/comments_mailers.js
+++ mailers/comments_mailers.js
@@ -6,12 +6,13 @@ const nodemailer = require('../config/nodemailer');
 // this is another way of exporting method module.exports.newComments = function(){}
 exports.newComment = (comment) => {
     console.log('Inside newComment mailer');
+    let htmlString = nodemailer.renderTemplate({comment: comment}, '/comments/new_comment.ejs')
 
     nodemailer.transporter.sendMail({
         from: '500053523@stu.upes.ac.in',
         to: comment.user.email,
         subject: 'New Comment Published',
-        html: '<h1>Yup, mail published </h1>'
+        html: htmlString
     },(err, info) => {
         if(err) { console.log('error in sending mail, ',err); return}
 
