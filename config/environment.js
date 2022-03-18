f885325f75c619d948dc800b9ce1bcfc1b89b09f
harshitsingh45 <harshitsingh45@gmail.com>
2022-03-18 14:24:09 +0530

    added production environment

--- config/environment.js
+++ config/environment.js
@@ -22,7 +22,24 @@ const development = {
 }
 
 const production  = {
-    name: 'production',
+    name: 'production' ,
+    assets_path: process.env.CODEIAL_ASSET_PATH,
+    session_cookie_key: process.env.CODEIAL_SESSION_COOKIE_KEY,
+    db: process.env.CODEIAL_DB,
+    smtp: {
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.CODEIAL_GMAIL_USERNAME,
+            pass: process.env.CODEIAL_GMAIL_PASSWORD
+        }
+    },
+    google_client_ID: process.env.CODEIAL_GOOGLE_CLIENT_ID,
+    google_client_Secret: process.env.CODEIAL_GOOGLE_CLIENT_SECRET,
+    google_callbackURL: process.env.CODEIAL_GOOGLE_CALLBACK_URL,
+    jwt_secret: process.env.CODEIAL_JWT_SECRET,
 }
 
-module.exports = development;
+module.exports = eval(process.env.CODEIAL_ENVIRONMENT)== undefined ? development : eval(process.env.CODEIAL_ENVIRONMENT);
